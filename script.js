77e2482df959ef3ccdd7d6e09dede8a7275ea7b6
unknown <aipz3562@gmail.com>
2024-12-15 21:25:22 -0800

    abc

---
+++ script.js
@@ -0,0 +1,11 @@
+//Write code in JavaScript to display all the even numbers till the number from 0 to 10. Output: 0 2 4 6 8 10 so it will display till 10 because 11 is an odd number. After completing the development host it to the Github and submit the live URL. If you have any doubt regarding this assignment please don't hesitate to ask in the Discordl. Thank you.
+
+
+
+for(let i=0; i<=10; i++)
+{
+    if (i%2==0){
+        console.log(i)
+    }
+}
+    
